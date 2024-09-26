364792e5440a8013d87a959ed3ba84166272a931
Kaarel <kaarel.viilvere@voco.ee>
2024-09-26 14:33:39 +0300

    Ylesanne 1.3 - koos kasutaja sisendiga

--- yl1.3.js
+++ yl1.3.js
@@ -2,3 +2,19 @@ let astmeAlus = 2;
 let astendaja = 4;
 let vastus = astmeAlus ** astendaja;
 console.log(vastus); 
+
+
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Sisestage astme alus: ", astmeAlus => {
+  rl.question("Sisestage astendaja: ", astendaja => {
+      let tulemus = astmeAlus ** astendaja;
+      console.log(tulemus);
+      rl.close();
+    });
+});
