7a418a39fe7065910bbde4d1eef1ef1848b471d6
Kaarel <kaarel.viilvere@voco.ee>
2024-09-26 15:29:48 +0300

    Ylesanne1.5

---
+++ yl1.5.js
@@ -0,0 +1,14 @@
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Sisestage ainepunktide arv :", ainepunktid =>{
+    rl.question("Sisestage nädalate arv :", nädalad => {
+        let ajakulu = 26;
+        let 
+    });
+});
+
